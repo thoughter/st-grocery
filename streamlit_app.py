6e5e3bd0564032b0e5e3fbd05a6d857eff1b4e89
Jack Yang <westc@DESKTOP-HA70RVO.localdomain>
2024-02-25 18:43:14 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,15 @@ import streamlit as st
 
 st.header('食材采购助手', divider='rainbow')
 
+niu_yang_rou=st.subheader('牛羊肉',divider='gray')
+zhou_rou=st.subheader('猪肉',divider='gray')
+ji_rou=st.subheader('鸡肉',divider='gray')
+
+hai_xian=st.subheader('海鲜',divider='gray')
 shui_zhu_yu=st.checkbox('水煮鱼')
+
+qing_cai=st.subheader('青菜',divider='gray')
+mian_shi=st.subheader('面食',divider='gray')
+
+
 
