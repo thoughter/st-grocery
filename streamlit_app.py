7b372b793c48a761c8ec9c357ecb3fef265aaa5c
Jack Yang <westc@DESKTOP-HA70RVO.localdomain>
2024-02-25 19:45:20 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,45 @@
 import streamlit as st
 
-st.header('食材采购助手', divider='rainbow')
+st.title('食材采购助手')
 
-niu_yang_rou=st.subheader('牛羊肉',divider='gray')
-zhou_rou=st.subheader('猪肉',divider='gray')
-ji_rou=st.subheader('鸡肉',divider='gray')
+###
+st.header('请选择菜单', divider='rainbow')
 
-hai_xian=st.subheader('海鲜',divider='gray')
-shui_zhu_yu=st.checkbox('水煮鱼')
+# Beef and Goat
+st.subheader('牛羊肉',divider='gray')
 
-qing_cai=st.subheader('青菜',divider='gray')
-mian_shi=st.subheader('面食',divider='gray')
+# Pork
+st.subheader('猪肉',divider='gray')
+col1, col2, col3 = st.columns(3)
+col1.checkbox('test1')
+
+# Chicken
+st.subheader('鸡肉',divider='gray')
+
+# Seafood
+st.subheader('海鲜',divider='gray')
+col1, col2, col3 = st.columns(3)
+shui_zhu_yu=col1.checkbox('水煮鱼')
+you_men_da_xia=col2.checkbox('油焖大虾')
+
+# Vegetables
+st.subheader('青菜',divider='gray')
+
+# Mian Shi
+st.subheader('面食',divider='gray')
 
 
+### 
+st.header('建议采购清单', divider='rainbow')
 
+mylist=[]
+if(you_men_da_xia):
+    mylist.extend(['虾','姜','蒜','葱','白糖','生抽','料酒','盐'])
+
+
+mkdn=''
+uniqlist=list(set(mylist))
+for item in mylist:
+    mkdn+=f'- {item}\n'
+
+st.markdown(mkdn)
