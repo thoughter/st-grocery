f716f5709ad13ec6a3e68c808b37e700c685c95f
Jack Yang <westc@DESKTOP-HA70RVO.localdomain>
2024-02-25 20:12:51 -0500

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.subheader('牛羊肉',divider='gray')
 # Pork
 st.subheader('猪肉',divider='gray')
 col1, col2, col3 = st.columns(3)
-col1.checkbox('test1')
+红烧肉=col1.checkbox('红烧肉')
 
 # Chicken
 st.subheader('鸡肉',divider='gray')
@@ -19,8 +19,8 @@ st.subheader('鸡肉',divider='gray')
 # Seafood
 st.subheader('海鲜',divider='gray')
 col1, col2, col3 = st.columns(3)
-shui_zhu_yu=col1.checkbox('水煮鱼')
-you_men_da_xia=col2.checkbox('油焖大虾')
+水煮鱼=col1.checkbox('水煮鱼')
+油焖大虾=col2.checkbox('油焖大虾')
 
 # Vegetables
 st.subheader('青菜',divider='gray')
@@ -33,13 +33,17 @@ st.subheader('面食',divider='gray')
 st.header('建议采购清单', divider='rainbow')
 
 mylist=[]
-if(you_men_da_xia):
+if(红烧肉):
+    mylist.extend(['五花肉','姜','八角','葱','桂皮','冰糖','生抽','料酒','盐','老抽'])
+if(油焖大虾):
     mylist.extend(['虾','姜','蒜','葱','白糖','生抽','料酒','盐'])
+if(水煮鱼):
+    mylist.extend(['鱼片','姜','蒜','淀粉','花椒','料酒','盐','干辣椒','豆瓣酱','黄豆芽'])
 
 
 mkdn=''
 uniqlist=list(set(mylist))
-for item in mylist:
+for item in uniqlist:
     mkdn+=f'- {item}\n'
 
 st.markdown(mkdn)
